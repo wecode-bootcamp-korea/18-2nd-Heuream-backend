4ef5e6f7632ec23ea9f0aa43d81b4fd8c479f848
gyudong <zlsrbrhd@gmail.com>
2021-03-29 17:31:43 +0900

    Add : 초기세팅 완료

---
+++ heuream/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for heuream project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'heuream.settings')
+
+application = get_asgi_application()
