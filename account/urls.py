d3f7f392be5d03645a39830763801e4dc9e91d36
gyudong <zlsrbrhd@gmail.com>
2021-04-01 20:39:14 +0900

    Add : 카카오 회원가입 로그인, 테스트 구현

--- account/urls.py
+++ account/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import UserSignIn, UserSignUp
+from .views import UserSignIn, UserSignUp, KakaoSignin
 
 urlpatterns = [
-   path('/signin', UserSignIn.as_view()),
-   path('/signup', UserSignUp.as_view()),
+    path('/signin', UserSignIn.as_view()),
+    path('/signup', UserSignUp.as_view()),
+    path('/kakaosignin', KakaoSignin.as_view()),
+
 ]
