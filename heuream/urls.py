9693e28c589e145c21fae0aae6643ce241d641d4
gyudong <zlsrbrhd@gmail.com>
2021-04-01 20:09:36 +0900

    Modify : 변수명 수정, 가격 수정

--- heuream/urls.py
+++ heuream/urls.py
@@ -16,5 +16,6 @@ Including another URLconf
 from django.urls import path, include
 
 urlpatterns = [
-    path('product', include('product.urls'))
+    path('product', include('product.urls')),
+    path('bidding', include('bidding.urls')),
 ]
