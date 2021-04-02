0d6895cfb3c556848bcfcd58b85d0cada19251aa
gyudong <zlsrbrhd@gmail.com>
2021-04-02 16:19:51 +0900

    Modify : status 코드 변경

--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,77 @@
-from django.test import TestCase
+import jwt
+import json
+import bcrypt
 
-# Create your tests here.
+from django.test import TestCase, Client
+
+from account.models import User
+from my_settings    import SECRET_KEY, ALGORITHM
+
+class SignInTest(TestCase):
+    def setUp(self):
+        password = 'paper1594'
+        password = (bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())).decode('utf-8')
+        User.objects.create(email='gyudong1594@wecode.com', password=password)
+    
+    def tearDown(self):
+        User.objects.all().delete()
+
+    def test_signin_success(self):
+        client = Client()
+
+        user = {
+            'email':'gyudong1594@wecode.com',
+            'password':'paper1594'
+        }
+
+        reponse = client.post('/account/signin', json.dumps(user), content_type='application/json')
+        access_token = jwt.encode({'user_id':5}, SECRET_KEY, ALGORITHM)
+
+        self.assertEqual(reponse.status_code, 200)
+        self.assertEqual(reponse.json(), {'message':'SUCCESS', 'access_token':access_token})
+    
+    def test_signin_invalid_password(self):
+        client = Client()
+
+        user = {
+            'email':'gyudong1594@wecode.com',
+            'password':'error_password'
+        }
+
+        reponse = client.post('/account/signin', json.dumps(user), content_type='application/json')
+
+        self.assertEqual(reponse.status_code, 400)
+        self.assertEqual(reponse.json(), {'message':'INVALID_USER'})
+
+    def test_signin_invalid_email(self):
+        client = Client()
+
+        user = {
+            'email':'error_email',
+            'password':'paper1594'
+        }
+
+        reponse = client.post('/account/signin', json.dumps(user), content_type='application/json')
+
+        self.assertEqual(reponse.status_code, 400)
+        self.assertEqual(reponse.json(), {'message':'INVALID_USER'})
+
+    def test_signin_key_error(self):
+        client = Client()
+
+        user = {
+            'password':'no_email'
+        }
+
+        reponse = client.post('/account/signin', json.dumps(user), content_type='application/json')
+
+        self.assertEqual(reponse.status_code, 400)
+        self.assertEqual(reponse.json(), {'message':'KEY_ERROR'})
+
+    def test_signin_invalid_json(self):
+        client = Client()
+
+        reponse = client.post('/account/signin')
+
+        self.assertEqual(reponse.status_code, 400)
+        self.assertEqual(reponse.json(), {'message':'INVALID_JSON'})
--- account/views.py
+++ account/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+import jwt
+import json
+import bcrypt
 
-# Create your views here.
+from django.views import View
+from django.http  import JsonResponse
+
+from account.models import User
+from my_settings    import SECRET_KEY, ALGORITHM
+
+class UserSignIn(View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+
+            email    = data["email"]
+            password = data["password"]
+            
+            if User.objects.filter(email = email).exists():
+                user = User.objects.get(email = email)
+
+                if bcrypt.checkpw(password.encode('utf-8'), user.password.encode('utf-8')):
+                    access_token = jwt.encode({'user_id': user.id}, SECRET_KEY, ALGORITHM)
+                    return JsonResponse({"message":"SUCCESS", 'access_token':access_token}, status = 200)
+                                
+                else:
+                    return JsonResponse({"message":"INVALID_USER"}, status = 401)    
+            else:
+                return JsonResponse({"message":"INVALID_USER"}, status = 401)
+                
+        except KeyError:
+            return JsonResponse({"message":"KEY_ERROR"}, status = 400)
+        
+        except json.decoder.JSONDecodeError:
+            return JsonResponse({"message": "INVALID_JSON"}, status = 400)
