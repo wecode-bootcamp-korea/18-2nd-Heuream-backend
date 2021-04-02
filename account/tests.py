11041a4c6ce54f95b1d8ec76dba1cccf847cc34c
gyudong <zlsrbrhd@gmail.com>
2021-04-02 16:01:06 +0900

    Modify : create preferred size 추가

--- account/tests.py
+++ account/tests.py
@@ -1,20 +1,101 @@
+import re
 import jwt
 import json
 import bcrypt
+import django
 
-from django.test import TestCase, Client
+from django.test  import TestCase, Client, TransactionTestCase
+from django.http  import JsonResponse
+from django.db    import transaction
 
 from account.models import User
 from my_settings    import SECRET_KEY, ALGORITHM
+
+class SignUpTest(TransactionTestCase):
+    def setUp(self):
+        with transaction.atomic():
+            User.objects.create(email='gyudong1594@wecode.com', password='paper1594@')
+    
+    def tearDown(self):
+        User.objects.all().delete()
+
+    def test_signup_success(self):
+        client = Client()
+
+        user = {
+            'email':'test@test.com',
+            'password':'testpSassword@1'
+        }
+
+        reponse = client.post('/account/signup', json.dumps(user), content_type='application/json')
+
+        self.assertEqual(reponse.status_code, 201)
+        self.assertEqual(reponse.json(), {'message':'SUCCESS'})
+    
+    def test_signup_invalid_email(self):
+        client = Client()
+
+        user = {
+            'email':'testtest.com',
+            'password':'testpassword@1'
+        }
+
+        reponse = client.post('/account/signup', json.dumps(user), content_type='application/json')
+
+        self.assertEqual(reponse.status_code, 400)
+        self.assertEqual(reponse.json(), {'message':'INVALID_EMAIL'})
+
+    def test_signup_invalid_password(self):
+        client = Client()
+
+        user = {
+            'email':'tes@test.com',
+            'password':'testpassword'
+        }
+
+        reponse = client.post('/account/signup', json.dumps(user), content_type='application/json')
+
+        self.assertEqual(reponse.status_code, 400)
+        self.assertEqual(reponse.json(), {'message':'INVALID_PASSWORD'})
+
+    def test_signup_key_error(self):
+        client = Client()
+
+        user = {
+            'email':'testtest.com'
+        }
+
+        reponse = client.post('/account/signup', json.dumps(user), content_type='application/json')
+
+        self.assertEqual(reponse.status_code, 400)
+        self.assertEqual(reponse.json(), {'message':'KEY_ERROR'})
+
+    def test_signup_invalid_json(self):
+        client = Client()
+
+        reponse = client.post('/account/signup')
+
+        self.assertEqual(reponse.status_code, 400)
+        self.assertEqual(reponse.json(), {'message':'INVALID_JSON'})
+
+    def test_signup_registered_email(self):
+        client = Client()
+
+        user = {
+            'email':'gyudong1594@wecode.com',
+            'password':'testpasswoSrd@1'
+        }
+
+        reponse = client.post('/account/signup', json.dumps(user), content_type='application/json')
+
+        self.assertEqual(reponse.status_code, 400)
+        self.assertEqual(reponse.json(), {'message':'REGISTERED_EMAIL'})
 
 class SignInTest(TestCase):
     def setUp(self):
         password = 'paper1594'
         password = (bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())).decode('utf-8')
         User.objects.create(email='gyudong1594@wecode.com', password=password)
-    
-    def tearDown(self):
-        User.objects.all().delete()
 
     def test_signin_success(self):
         client = Client()
@@ -40,7 +121,7 @@ class SignInTest(TestCase):
 
         reponse = client.post('/account/signin', json.dumps(user), content_type='application/json')
 
-        self.assertEqual(reponse.status_code, 400)
+        self.assertEqual(reponse.status_code, 401)
         self.assertEqual(reponse.json(), {'message':'INVALID_USER'})
 
     def test_signin_invalid_email(self):
@@ -53,7 +134,7 @@ class SignInTest(TestCase):
 
         reponse = client.post('/account/signin', json.dumps(user), content_type='application/json')
 
-        self.assertEqual(reponse.status_code, 400)
+        self.assertEqual(reponse.status_code, 401)
         self.assertEqual(reponse.json(), {'message':'INVALID_USER'})
 
     def test_signin_key_error(self):
@@ -67,7 +148,7 @@ class SignInTest(TestCase):
 
         self.assertEqual(reponse.status_code, 400)
         self.assertEqual(reponse.json(), {'message':'KEY_ERROR'})
-
+    
     def test_signin_invalid_json(self):
         client = Client()
 
--- account/views.py
+++ account/views.py
@@ -1,12 +1,52 @@
+import re
 import jwt
 import json
 import bcrypt
+import django
 
 from django.views import View
 from django.http  import JsonResponse
 
 from account.models import User
+from product.models import Size
 from my_settings    import SECRET_KEY, ALGORITHM
+
+MIN_PASSWORD_LENGTH = 8
+MAX_PASSWORD_LENGTH = 16
+
+class UserSignUp(View):
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+
+            password  = data['password']
+            email     = data['email']
+            size      = data.get('size', None)
+                
+            if not (re.match('^[a-zA-Z0-9+-_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$', email)):
+                return JsonResponse({"message":"INVALID_EMAIL"}, status = 400)
+
+            if not (re.findall('^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,16}', password)):
+                return JsonResponse({"message":"INVALID_PASSWORD"}, status = 400)
+
+            password_hashed = (bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())).decode('utf-8')
+                
+            User.objects.create(
+                email=email,
+                password=password_hashed,
+                preferred_size=size
+            )
+
+            return JsonResponse({'message':'SUCCESS'}, status = 201)
+        
+        except KeyError:
+            return JsonResponse({"message": "KEY_ERROR"}, status = 400)
+        
+        except json.decoder.JSONDecodeError:
+            return JsonResponse({"message": "INVALID_JSON"}, status = 400)
+        
+        except django.db.utils.IntegrityError:
+            return JsonResponse({'message':'REGISTERED_EMAIL'}, status = 400)
 
 class UserSignIn(View):
     def post(self, request):
