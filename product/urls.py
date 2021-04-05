caf0538f50b0432e35b8076a1d9cf755c2c852d4
Won <jungwon1423@gamil.com>
2021-04-05 10:25:07 +0900

    Add: WishlistView 생성 Add: WishlistView get 메소드 생성 Add: WishlistView post 메소드 생성 Add: unite test 생성 Modify: size_name return 값에 추가 Refactor: 리뷰수정, unit test 성공

--- product/urls.py
+++ product/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from product.views import ProductFilterView
+from product.views import ProductFilterView, WishlistView
 
 urlpatterns = [
-    path('', ProductFilterView.as_view())
+    path('', ProductFilterView.as_view()),
+    path('/<int:product_id>', WishlistView.as_view())
 ]
